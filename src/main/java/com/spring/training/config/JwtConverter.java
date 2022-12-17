1f5452440bcf849c4660e7383b4d555665a3aae4
mamadou lamine ba <laminba2003@gmail.com>
2022-12-17 18:34:02 +0000

    improvements

--- src/main/java/com/spring/training/config/JwtConverter.java
+++ src/main/java/com/spring/training/config/JwtConverter.java
@@ -3,10 +3,11 @@ package com.spring.training.config;
 import com.spring.training.domain.User;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.BearerTokenAuthentication;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,14 +20,16 @@ public class JwtConverter implements Converter<Jwt, AbstractAuthenticationToken>
 
     @Override
     public AbstractAuthenticationToken convert(Jwt jwt) {
-        User user = new User(jwt.getClaim(GIVEN_NAME),
-                jwt.getClaim(FAMILY_NAME),
-                jwt.getClaim(EMAIL));
         List<String> roles = Optional.ofNullable(jwt.getClaimAsStringList(ROLES)).orElse(new ArrayList<>());
         List<GrantedAuthority> authorities = roles.stream().
-                map(SimpleGrantedAuthority::new)
+                map(authority -> new SimpleGrantedAuthority("ROLE_" + authority))
                 .collect(Collectors.toList());
-        return new UsernamePasswordAuthenticationToken(user, null, authorities);
+        User user = new User(jwt.getClaim(GIVEN_NAME),
+                jwt.getClaim(FAMILY_NAME),
+                jwt.getClaim(EMAIL), authorities);
+        OAuth2AccessToken token = new OAuth2AccessToken(OAuth2AccessToken.TokenType.BEARER,
+                jwt.getTokenValue(), jwt.getIssuedAt(), jwt.getExpiresAt());
+        return new BearerTokenAuthentication(user, token, authorities);
     }
 
 }
